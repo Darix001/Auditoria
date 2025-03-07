734dd0f9fa93340e4a550be80e85f98a211659f9
Darix001 <100180688+Darix001@users.noreply.github.com>
2025-03-07 15:21:51 -0400

    Update find_duplicate.py

--- find_duplicate.py
+++ find_duplicate.py
@@ -1,7 +1,13 @@
 import itertools, collections
-def main(lista=range(100)):
-	value=1
-	return next(filter(value.__gt__,map(lista.count,lista)),None)
+def main(lista):
+	vistos = set()
+	dups = []
+	for x in a:
+    		if x in seen:
+        		dups.append(x)
+		else:
+			seen.add(x)
+        		
 
 if __name__ == '__main__':
 	print(main((1,2,3,4,5,6,7,8,9,10,2,3,4,5,6,7,8,6,4,32)))
